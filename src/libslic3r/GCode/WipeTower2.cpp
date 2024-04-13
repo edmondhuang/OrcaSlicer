7d4a3dc1fcbe51ebb5a1d52dd88fa063dc18cb9f
edmondhuang <hestiahuang@qq.com>
2024-04-14 02:54:15 +0800

    test

--- src/libslic3r/GCode/WipeTower2.cpp
+++ src/libslic3r/GCode/WipeTower2.cpp
@@ -832,6 +832,7 @@ WipeTower::ToolChangeResult WipeTower2::tool_change(size_t tool)
         toolchange_Change(writer, tool, m_filpar[tool].material); // Change the tool, set a speed override for soluble and flex materials.
         toolchange_Load(writer, cleaning_box);
         writer.travel(writer.x(), writer.y()-m_perimeter_width); // cooling and loading were done a bit down the road
+        writer.append(";GGGGGGGGGG\n");
         toolchange_Wipe(writer, cleaning_box, wipe_volume);     // Wipe the newly loaded filament until the end of the assigned wipe area.
         ++ m_num_tool_changes;
     } else
