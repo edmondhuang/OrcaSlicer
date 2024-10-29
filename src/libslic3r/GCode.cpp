c85bac6d9af473a8475d674c05af0936b89f0fb1
edmondhuang <hestiahuang@qq.com>
2024-10-30 02:28:28 +0800

    Update GCode.cpp

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -3763,23 +3763,24 @@ LayerResult GCode::process_layer(
 
         // Transition from 1st to 2nd layer. Adjust nozzle temperatures as prescribed by the nozzle dependent
         // nozzle_temperature_initial_layer vs. temperature settings.
-        for (const Extruder &extruder : m_writer.extruders()) {
-            if (print.config().single_extruder_multi_material.value && extruder.id() != m_writer.extruder()->id())
-                // In single extruder multi material mode, set the temperature for the current extruder only.
-                continue;
-            int temperature = print.config().nozzle_temperature.get_at(extruder.id());
-            int temperature0 = print.config().nozzle_temperature.get_at(0);
-            int temperature1 = print.config().nozzle_temperature.get_at(1);
-            int temperature2 = print.config().nozzle_temperature.get_at(2);
-            int temperature3 = print.config().nozzle_temperature.get_at(3);
-            if (temperature > 0 && temperature != print.config().nozzle_temperature_initial_layer.get_at(extruder.id())) { //Edmond
-                gcode += "; Set the 2nd layer+ temperature\n";
-                gcode += m_writer.set_temperature(temperature, false, 0);
-                gcode += m_writer.set_temperature(temperature, false, 1);
-                gcode += m_writer.set_temperature(temperature, false, 2);
-                gcode += m_writer.set_temperature(temperature, false, 3);
-            }
-        }
+//        for (const Extruder &extruder : m_writer.extruders()) {
+//            if (print.config().single_extruder_multi_material.value && extruder.id() != m_writer.extruder()->id())
+//                // In single extruder multi material mode, set the temperature for the current extruder only.
+//                continue;
+//            int temperature = print.config().nozzle_temperature.get_at(extruder.id());
+//            if (temperature > 0 && temperature != print.config().nozzle_temperature_initial_layer.get_at(extruder.id())) { //Edmond
+//                gcode += "; Set the 2nd layer+ temperature\n";
+//                gcode += m_writer.set_temperature(temperature, false, 0);
+//                gcode += m_writer.set_temperature(temperature, false, 1);
+//                gcode += m_writer.set_temperature(temperature, false, 2);
+//                gcode += m_writer.set_temperature(temperature, false, 3);
+//            }
+//        }
+      gcode += "; Set the 2nd layer+ temperature\n";
+      //gcode += m_writer.set_temperature(m_config.nozzle_temperature.get_at(extruder_id)), false, 0);
+      gcode += m_writer.set_temperature(m_config.nozzle_temperature.get_at(1)), false, 1);
+      gcode += m_writer.set_temperature(m_config.nozzle_temperature.get_at(2)), false, 2);
+      gcode += m_writer.set_temperature(m_config.nozzle_temperature.get_at(3)), false, 3);
 
         // BBS
         int bed_temp = get_bed_temperature(first_extruder_id, false, print.config().curr_bed_type);
